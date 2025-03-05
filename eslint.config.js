faf9fe86cf26f4d214313b268ac98817f17267c5
Rusty <rusty3925@gmail.com>
2025-03-04 21:39:45 -0800

    add bristlefrost eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
+import eslintConfigPrettier from 'eslint-config-prettier';
+import bristlefrostConfig from '@bristlefrost/eslint-config';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+	...bristlefrostConfig,
+	{
+		files: ['src/**/*.ts'],
+		languageOptions: {
+			parser: tsParser,
+		},
+		plugins: {
+			tsPlugin,
+		},
+	},
+	eslintConfigPrettier,
 ];
