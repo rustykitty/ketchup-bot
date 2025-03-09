f1547c18bb40aaf21de1e59c948e37b705c292c2
Rusty <rusty3925@gmail.com>
2025-03-09 09:54:07 -0700

    sql stuff

--- database_schema.sql
+++
@@ -1,6 +0,0 @@
--- not a code file, just where I keep my DB schemas.
-
-CREATE TABLE user_data (
-    id VARCHAR(20) PRIMARY KEY,
-    ketchup INT NOT NULL,
-);
---
+++ sql_stuff.sql
@@ -0,0 +1,22 @@
+-- CREATE TABLE user_data (
+--     id VARCHAR(20) PRIMARY KEY,
+--     ketchup INT NOT NULL DEFAULT 0,
+--     last_daily DATE NOT NULL DEFAULT '1970-01-01',
+-- );
+
+-- migration
+BEGIN TRANSACTION;
+
+CREATE TABLE user_data_old AS SELECT * FROM user_data;
+DROP TABLE user_data;
+CREATE TABLE user_data (
+    id VARCHAR(20) PRIMARY KEY,
+    ketchup INT NOT NULL DEFAULT 0,
+    last_daily DATE NOT NULL DEFAULT '1970-01-01'
+);
+INSERT INTO user_data (id, ketchup, last_daily)
+SELECT id, ketchup, '1970-01-01' FROM user_data_old;
+
+DROP TABLE user_data_old;
+
+COMMIT;
