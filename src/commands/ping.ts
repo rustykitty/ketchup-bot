abea31df21cbab751f7babc6193878d8426d9fd4
Rusty <rusty3925@gmail.com>
2025-05-18 07:34:10 -0700

    add latency to ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -7,10 +7,16 @@ export const ping: Command = {
         description: 'Replies with pong!',
     },
     execute: async (interaction) => {
+        // Parse timestamp from snowflake
+        const ts: number = Number(
+            (BigInt(interaction.id) >> 22n) + 1420070400000n,
+        );
+        const now = Date.now();
         return {
             type: DAPI.InteractionResponseType.ChannelMessageWithSource,
             data: {
-                content: 'Pong!',
+                content: `Pong!
+One-way latency: ${now - ts}ms`,
             },
         };
     },
