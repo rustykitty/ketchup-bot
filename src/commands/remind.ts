b555065dca9d843bd81ffef5c5d21e9352d0d0a7
Rusty <rusty3925@gmail.com>
2025-05-18 19:15:01 -0700

    have all the remind commands in one object

--- src/commands/remind.ts
+++ src/commands/remind.ts
@@ -4,92 +4,93 @@ import * as chrono from 'chrono-node';
 import { Command, SubcommandExecute } from './command.js';
 import { getSubcommandOptions, getSubcommand, getUser } from '../utility.js';
 
-const remind_set: SubcommandExecute = async (interaction, env, ctx) => {
-    const db: D1Database = env.DB;
-    const user_id = getUser(interaction);
-    const { time, message } = getSubcommandOptions(interaction, 'set');
-    const date: Date | null = chrono.parseDate(time.value as string, {
-        timezone: 'PDT',
-    });
-    if (date === null) {
+const subcommands: Record<string, SubcommandExecute> = {
+    set: async (interaction, env, ctx) => {
+        const db: D1Database = env.DB;
+        const user_id = getUser(interaction);
+        const { time, message } = getSubcommandOptions(interaction, 'set');
+        const date: Date | null = chrono.parseDate(time.value as string, {
+            timezone: 'PDT',
+        });
+        if (date === null) {
+            return {
+                type: DAPI.InteractionResponseType.ChannelMessageWithSource,
+                data: {
+                    content: `Invalid date format.`,
+                },
+            };
+        }
+        const ts = +date;
+        if (ts <= Date.now()) {
+            return {
+                type: DAPI.InteractionResponseType.ChannelMessageWithSource,
+                data: {
+                    content: `You can't set a reminder in the past!`,
+                },
+            };
+        }
+        db.prepare(`INSERT INTO reminders (user_id, message, timestamp) VALUES (?, ?, ?)`)
+            .bind(user_id, message.value, ts)
+            .run();
         return {
             type: DAPI.InteractionResponseType.ChannelMessageWithSource,
             data: {
-                content: `Invalid date format.`,
+                content: `I will remind you about "${message.value}" <t:${ts}:R>.`,
             },
         };
-    }
-    const ts = +date;
-    if (ts <= Date.now()) {
-        return {
-            type: DAPI.InteractionResponseType.ChannelMessageWithSource,
-            data: {
-                content: `You can't set a reminder in the past!`,
-            },
-        };
-    }
-    db.prepare(`INSERT INTO reminders (user_id, message, timestamp) VALUES (?, ?, ?)`)
-        .bind(user_id, message.value, ts)
-        .run();
-    return {
-        type: DAPI.InteractionResponseType.ChannelMessageWithSource,
-        data: {
-            content: `I will remind you about "${message.value}" <t:${ts}:R>.`,
-        },
-    };
-};
-
-const remind_list: SubcommandExecute = async (interaction, env, ctx) => {
-    const db: D1Database = env.DB;
-    const user_id = getUser(interaction);
-    const result: D1Result<RemindersRow> = await db
-        .prepare(`SELECT * FROM reminders WHERE user_id = ?`)
-        .bind(user_id)
-        .run();
-    if (result.results.length === 0) {
-        return {
-            type: DAPI.InteractionResponseType.ChannelMessageWithSource,
-            data: {
-                content: `You do not have any reminders set.`,
-            },
-        };
-    }
-    const remindersText = result.results.map((element, index, array) => {
-        const { id, message, timestamp } = element;
-        const date = new Date(timestamp * 1000);
-        return `- ${message} <t:${timestamp}:F> (ID: \`${id}\`)`;
-    });
-    return {
-        type: DAPI.InteractionResponseType.ChannelMessageWithSource as any,
-        data: {
-            content: `You have the following reminders:\n${remindersText.join('\n')}`,
-        },
-    };
-};
-const remind_remove: SubcommandExecute = async (interaction, env, ctx) => {
-    const db: D1Database = env.DB;
-    const user_id = getUser(interaction);
-    const { id } = getSubcommandOptions(interaction, 'remove');
-    const results = await db.batch([
-        db.prepare(`SELECT * FROM reminders WHERE user_id = ? AND id = ?`).bind(user_id, id.value),
-        db.prepare(`DELETE FROM reminders WHERE user_id = ? AND id = ?`).bind(user_id, id.value),
-    ]);
-    const reminder: RemindersRow = (results[0] as D1Result<RemindersRow>).results[0];
-    if (reminder) {
+    },
+    list: async (interaction, env, ctx) => {
+        const db: D1Database = env.DB;
+        const user_id = getUser(interaction);
+        const result: D1Result<RemindersRow> = await db
+            .prepare(`SELECT * FROM reminders WHERE user_id = ?`)
+            .bind(user_id)
+            .run();
+        if (result.results.length === 0) {
+            return {
+                type: DAPI.InteractionResponseType.ChannelMessageWithSource,
+                data: {
+                    content: `You do not have any reminders set.`,
+                },
+            };
+        }
+        const remindersText = result.results.map((element, index, array) => {
+            const { id, message, timestamp } = element;
+            const date = new Date(timestamp * 1000);
+            return `- ${message} <t:${timestamp}:F> (ID: \`${id}\`)`;
+        });
         return {
             type: DAPI.InteractionResponseType.ChannelMessageWithSource as any,
             data: {
-                content: `Removed reminder with ID \`${id.value}\` (was "${reminder.message}", set for <t:${reminder.timestamp}:F>)`,
+                content: `You have the following reminders:\n${remindersText.join('\n')}`,
             },
         };
-    } else {
-        return {
-            type: DAPI.InteractionResponseType.ChannelMessageWithSource as any,
-            data: {
-                content: `No reminder found with ID \`${id.value}\`. It may have already triggered or been removed.`,
-            },
-        };
-    }
+    },
+    remove: async (interaction, env, ctx) => {
+        const db: D1Database = env.DB;
+        const user_id = getUser(interaction);
+        const { id } = getSubcommandOptions(interaction, 'remove');
+        const results = await db.batch([
+            db.prepare(`SELECT * FROM reminders WHERE user_id = ? AND id = ?`).bind(user_id, id.value),
+            db.prepare(`DELETE FROM reminders WHERE user_id = ? AND id = ?`).bind(user_id, id.value),
+        ]);
+        const reminder: RemindersRow = (results[0] as D1Result<RemindersRow>).results[0];
+        if (reminder) {
+            return {
+                type: DAPI.InteractionResponseType.ChannelMessageWithSource as any,
+                data: {
+                    content: `Removed reminder with ID \`${id.value}\` (was "${reminder.message}", set for <t:${reminder.timestamp}:F>)`,
+                },
+            };
+        } else {
+            return {
+                type: DAPI.InteractionResponseType.ChannelMessageWithSource as any,
+                data: {
+                    content: `No reminder found with ID \`${id.value}\`. It may have already triggered or been removed.`,
+                },
+            };
+        }
+    },
 };
 
 export const remind: Command = {
@@ -137,11 +138,6 @@ export const remind: Command = {
         ],
     },
     execute(interaction, env, ctx) {
-        const subcommands: Record<string, SubcommandExecute> = {
-            set: remind_set,
-            list: remind_list,
-            remove: remind_remove,
-        };
         const subcommand = getSubcommand(interaction);
         if (!subcommand) {
             throw new Error('No subcommand found');
