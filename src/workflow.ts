43e80e7cac9a920d9536ea3ada7fb2a1b14f71f1
Rusty <rusty3925@gmail.com>
2025-05-23 14:19:54 -0700

    moving things from scheduled to workflow

--- src/workflow.ts
+++ src/workflow.ts
@@ -1,7 +1,38 @@
+import * as DAPI from 'discord-api-types/v10';
+
 import { WorkflowEntrypoint, WorkflowStep, WorkflowEvent } from 'cloudflare:workers';
 
-import { openDM } from './scheduled.js';
 import { sleep } from './utility.js';
+
+const cache: Record<string, string> = {};
+
+async function openDM(userId: string, env: Env): Promise<string> {
+    if (cache[userId]) return cache[userId];
+    while (true) {
+        const response = await fetch(`https://discord.com/api/v10/users/@me/channels`, {
+            method: 'POST',
+            body: JSON.stringify({ recipient_id: userId }),
+            headers: {
+                Authorization: `Bot ${env.DISCORD_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.status === 429) {
+            const { retry_after } = await response.json<{ retry_after: number }>();
+            await sleep(Math.trunc(retry_after * 1000));
+            continue;
+        } else if (!response.ok) {
+            console.error(`Failed to open DM with user ${userId}: ${response} ${response.statusText}`);
+            console.error(await response.text());
+            throw new Error(`Failed to open DM with user ${userId}`);
+        }
+        const channel = (await response.json<DAPI.APIChannel>()).id;
+        // eslint-disable-next-line require-atomic-updates
+        cache[userId] = channel;
+        return channel;
+    }
+}
 
 async function sendReminderDM(reminder: Reminder, env: Env): Promise<void> {
     const db: D1Database = env.DB;
