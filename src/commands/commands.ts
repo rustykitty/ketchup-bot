9cceb814a72deb856a60b55a07237e070cd7e817
Rusty <rusty3925@gmail.com>
2025-05-23 16:19:21 -0700

    remove `test` command

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -3,8 +3,7 @@ import ping from './ping.js';
 import echo from './echo.js';
 import currency from './currency.js';
 import remind from './remind.js';
-import test from './test.js';
 
-const commands: Command[] = [...ping, ...echo, ...currency, ...remind, ...test];
+const commands: Command[] = [...ping, ...echo, ...currency, ...remind];
 
 export default commands;
